a42c9d355c215300659c95bbb956fd65969e6c26
Kozloder <Kozloder@users.noreply.github.com>
2021-08-25 18:45:57 +0700

    fix

---
+++ Assets/TurnTheGameOn/SimpleTrafficSystem/Scripts/Core/Safety/JobSafetyContainer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traffic
+{
+    public class JobSafetyContainer<T> : IDisposable where T : struct, IDisposable
+    {
+        private T buffer1, buffer2;
+
+        private T readPointer;
+        private T writePointer;
+
+        private bool locked = false;
+
+        public JobSafetyContainer(Func<T> createFunc)
+        {
+            readPointer = buffer1 = createFunc();
+            writePointer = buffer2 = createFunc();
+        }
+
+        public void Lock()
+        {
+            locked = true;
+        }
+
+        public void Unlock()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            buffer1.Dispose();
+            buffer2.Dispose();
+        }
+        /*
+        public static implicit operator T(JobSafetyContainer<T> container) 
+        {
+            return container.locked ? container.writePointer : 
+        }
+        public T AsWrite() 
+        {
+            return contai
+        }*/
+    }
+}
---
+++ Assets/TurnTheGameOn/SimpleTrafficSystem/Scripts/Core/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Traffic
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T instance;
+
+        public static T Instance 
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = FindObjectOfType<T>();
+
+                    if (instance == null)
+                    {
+                        instance = new GameObject().AddComponent<T>();
+                    }
+                }
+
+                return instance;
+            }
+        }
+    }
+}
